380d93cc88efe596ffe8981dfdb03b480bfc2b37
Divya Gupta <divygupta0319@gmail.com>
2023-04-06 01:03:31 +0530

    Deleted properties loader config class as it was not needed and not working, loading properties file directly in controller class now, and IT WORKS!!

--- src/main/java/com/luv2code/springbootmvcdemo/StudentController.java
+++ src/main/java/com/luv2code/springbootmvcdemo/StudentController.java
@@ -2,20 +2,24 @@ package com.luv2code.springbootmvcdemo;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@PropertySource("classpath:countries.properties")
+@PropertySource("classpath:operatingsystems.properties")
 @RequestMapping("/student")
 public class StudentController {
+		
+	@Value("#{${countries}}")
+	private Map<String, String> countryOptions;
 	
-	@Autowired
-	private Environment env;
+	@Value("#{${opSys}}")
+	private Map<String, String> operatingSystemOptions;
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -27,10 +31,11 @@ public class StudentController {
 		theModel.addAttribute("student", theStudent);
 		
 		// add the country options to the model 
-		theModel.addAttribute("theCountryOptions", env.getProperty("countries"));
+		theModel.addAttribute("theCountryOptions", countryOptions);
+		
 		
 		// add the operating systems to the model
-		theModel.addAttribute("theOSOptions", env.getProperty("opSys"));
+		theModel.addAttribute("theOSOptions", operatingSystemOptions);
 		
 		return "student-form";
 		
