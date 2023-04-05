3ff8f82e313256d554d38e7525b3a56e4bb37ef0
Divya Gupta <divygupta0319@gmail.com>
2023-04-05 23:50:04 +0530

    Added all models and controllers from Spring MVC project

---
+++ src/main/java/com/luv2code/springbootmvcdemo/CustomerController.java
@@ -0,0 +1,61 @@
+package com.luv2code.springbootmvcdemo;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	// add an initbinder .. to convert trim input string
+	// remove leading and trailing whitespaces, and trim string down to null if has only whitespace
+	// resolve  issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		// defines a editor which will scan all strings present in form data
+		// and will trim leading and trailing white spaces
+		// true parameter tells Spring to trim string to null of it has only white spaces
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true); 
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel) {
+		
+		theModel.addAttribute("customer", new Customer()); // defining model attribute as "customer", which will be used by the form for data binding
+		
+		return "customer-form";
+		
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm(
+			// @Valid annotation used, to tell Spring to perform validation rules on this Customer object
+			// "customer" model attribute here is received from the form submission
+			@Valid @ModelAttribute("customer") Customer theCustomer,
+			BindingResult theBindingResult) { //  Spring will store the result of validation in BindingResult object
+		
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+		
+		System.out.println("Binding result: " + theBindingResult);
+		
+		System.out.println("\n\n\n");
+		
+		if(theBindingResult.hasErrors()) {
+			return "customer-form";
+		} else { 
+			return "customer-confirmation";
+		}
+	}
+
+}
---
+++ src/main/java/com/luv2code/springbootmvcdemo/Student.java
@@ -0,0 +1,82 @@
+package com.luv2code.springbootmvcdemo;
+
+import java.util.LinkedHashMap;
+
+public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private String country;
+	private String favoriteLanguage;
+	private String[] operatingSystems;
+
+	
+	private LinkedHashMap<String, String> favoriteLanguageOptions;
+	
+	
+	public Student() {
+		favoriteLanguageOptions = new LinkedHashMap<>();
+		favoriteLanguageOptions.put("Java", "Java");
+		favoriteLanguageOptions.put("Python", "Python");
+		favoriteLanguageOptions.put("C#", "C#");
+		favoriteLanguageOptions.put("Ruby", "Ruby");
+		
+		
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public String getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+
+	public LinkedHashMap<String, String> getFavoriteLanguageOptions() {
+		return favoriteLanguageOptions;
+	}
+
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+
+	
+
+}
