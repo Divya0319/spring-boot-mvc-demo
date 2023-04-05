6df891f80673dfb3a6830a0cc8f5cb591875cd49
Divya Gupta <divygupta0319@gmail.com>
2023-04-05 23:39:50 +0530

    Initial commit of Spring Boot mvc project with all default configuration of Spring boot

---
+++ src/test/java/com/luv2code/springbootmvcdemo/SpringBootMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springbootmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
